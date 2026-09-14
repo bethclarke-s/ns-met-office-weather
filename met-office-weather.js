1c356d2b244b5f5b21247fb7064c288776ff0811
bethclarke-s <beth.clarke@softwire.com>
2026-09-14 16:11:31 +0100

    Process the API call results and print out next 3 hours of temperature and weather types

--- met-office-weather.js
+++ met-office-weather.js
@@ -20,28 +20,85 @@ function get_longitude_latitude_from_user() {
 
 }
 
-function get_weather_from_longitude_latitude(longitude, latitude) {
+function get_hourly_data_from_API_response(response, hour){
 
-    const fetchData = async () => {
-        try {
-            const url = `https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=${longitude}&longitude=${latitude}`;
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {"apikey": process.env.API_KEY}
-            });
-            const responseJson = await response.json();
-            console.log(responseJson)
-        } catch (error) {
-            console.error(error)
-        } finally {
-            console.log("Request complete")
-        } 
+    return response.features[0].properties.timeSeries[hour - 1];
+
 }
 
-fetchData()
+function determine_weather_type_from_hourly_data(data) {
+
+    const tol = 1;
+    const windy_tol = 10; // TODO: set this to something more reasonable
+
+    if (data.totalSnowAmount > tol){
+        return 'snowing';
+    } else if (data.precipitationRate > tol){
+        return 'raining';
+    } else if (data.windGustSpeed10m > windy_tol){
+        return 'windy';
+    } else {
+        return 'sunny';
+    }
+
+}
+
+function get_time_from_hourly_data(data){
+
+    const dateAndTime = data.time;
+    const time = dateAndTime.substr(11,5);
+    return time;
+
+}
+
+
+function print_weather_report_from_API_response(response, hour) {
+
+    const hourlyData = get_hourly_data_from_API_response(response, hour);
+
+    const time = get_time_from_hourly_data(hourlyData);
+    const temperature = hourlyData.feelsLikeTemperature;
+    const weather_type = determine_weather_type_from_hourly_data(hourlyData);
+        
+    console.log(`At ${time}, the temperature will feel like ${temperature}C and it will be ${weather_type}. `)
+        
+    if (weather_type === 'raining') {
+        console.log('Bring an umbrella!')
+    }
+}
+
+function find_temperature_from_API_response(response){
+
+    const hoursToDisplay = 3;
+
+    for (let hour = 1; hour <= hoursToDisplay; hour++) {
+        
+        print_weather_report_from_API_response(response,hour);
+
+    }
+
 }
 
 
 //const [longitude, latitude] = get_longitude_latitude_from_user();
 const [longitude, latitude] = [51.5539, -0.1446];
-get_weather_from_longitude_latitude(longitude,latitude)
+
+const makeAPICall = async () => {
+    try {
+        const url = `https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=${longitude}&longitude=${latitude}`;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {"apikey": process.env.API_KEY}
+        });
+        const responseJson = await response.json();
+        //console.log(responseJson)
+        find_temperature_from_API_response(responseJson)
+    } catch (error) {
+        console.error(error)
+    } finally {
+        console.log("Request complete")
+    } 
+
+}
+
+makeAPICall()
