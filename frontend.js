fd565dacc12f7011d72028708750d5f180b8c641
bethclarke-s <beth.clarke@softwire.com>
2026-09-15 16:06:08 +0100

    Moving API files into src for App

--- frontend.js
+++ frontend.js
@@ -1,5 +1,5 @@
-import { generate_3_hour_weather_report_from_longitude_latitude } from './met_office_API.js';
-import { get_longitude_latitude_from_postcode } from './postcode_API.js';
+import { generate_3_hour_weather_report_from_longitude_latitude } from './metoffice/src/met_office_API.js';
+import { get_longitude_latitude_from_postcode } from './metoffice/src/postcode_API.js';
 import * as readline from 'node:readline';
  
 async function get_longitude_latitude_from_user() {
@@ -27,7 +27,6 @@ async function main() {
     const [longitude, latitude] = await get_longitude_latitude_from_user();
 
     generate_3_hour_weather_report_from_longitude_latitude(longitude, latitude);
-
 }
 
 main();
