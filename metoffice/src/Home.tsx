86ccb898c40291540f20980e04a774582ba97e51
bethclarke-s <beth.clarke@softwire.com>
2026-09-22 15:21:48 +0100

    Added random number generator and reset buttons

--- metoffice/src/Home.tsx
+++ metoffice/src/Home.tsx
@@ -3,22 +3,33 @@ import './index.css'
 
 function Home() {
   const [randomNumber, setRandomNumber] = useState<number>();
+  const [randomNumberButtonVisible, setRandomNumberButtonVisible] = useState<boolean>(true);
 
   const updateRandomNumber = (): React.ReactElement => {
     const num = Math.floor((Math.random() * 1000)+1);
     setRandomNumber(num);
-    console.log(randomNumber)
   } 
 
   const updateRandomNumberButtonVisible = (): React.ReactElement => {
     setRandomNumberButtonVisible(!randomNumberButtonVisible);
   }
 
+  const handleRandomButtonClick = (): React.ReactElement => {
+    updateRandomNumber();
+    updateRandomNumberButtonVisible();
+  }
+
   return <>
   <h1>Welcome to my Home Page!</h1>
   <p> Beth is testing how to do react...</p>
-  <button onClick = {updateRandomNumber}> Click to generate a random number! </button>
-  <h1>{randomNumber}</h1>
+  {randomNumberButtonVisible ? (
+    <button onClick = {handleRandomButtonClick}> Click to generate a random number! </button>
+    ) : (
+    <> 
+      <h1>{randomNumber}</h1> 
+      <button onClick = {updateRandomNumber}> Choose a new number (fun!)</button>
+      <button onClick = {updateRandomNumberButtonVisible}> Reset (less fun) </button> 
+    </>)}
   </>;
 }
 
